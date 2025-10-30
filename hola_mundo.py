724489825161cd66a5e78ef6c7a3d086caac72bb
vittonemax <vittone.maximiliano@gmail.com>
2025-10-30 19:34:45 -0300

    versionado1

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,3 +1,13 @@
 print("primer archivo python")
 nombre = input("¿Cómo te llamas? ")
 print(f"¡Hola, {nombre}! Bienvenido al mundo de la automatización con Python.!ss!!!!!")
+
+
+numero = float(input("Ingresa un número: "))
+
+if numero > 0:
+    print("El número es positivo.")
+elif numero < 0:
+    print("El número es negativo.")
+else:
+    print("El número es cero.")
